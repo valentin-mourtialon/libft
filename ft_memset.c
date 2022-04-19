4c2c5f4e6319acd466d1f79c159923449b99d237
valentin <valentinmourtialon@protonmail.com>
2022-04-19 13:53:57 +0200

    ft_memset done (norme not checked)

--- ft_memset.c
+++ ft_memset.c
@@ -3,21 +3,11 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
-	//int	*src;
-		
-	i = 0;
-	//src = s;
-	while (i < n)
-	{
-		((int *)s)[i] = c;
-		//printf("i = %ld, s[%ld] = %d\n", i, i, ((int *)s)[i]);
-		i++;
-		//*src++ = c;
-	}
+	while (n-- > 0)
+		*((unsigned char *)s++) = (unsigned char)c;
 	return (s);
 }
-
+/*
 #include <stdlib.h>
 #include <string.h>
 
@@ -25,18 +15,10 @@ int	main(int ac, char **av)
 {
 	if (ac == 4)
 	{
-		char	*str;
-		int	i;
+		printf("%s\n", (char *)memset(av[1], atoi(av[2]), atoi(av[3])));
+		printf("%s", (char *)memset(av[1], atoi(av[2]), atoi(av[3])));
 
-		str = (char *)ft_memset(av[1], atoi(av[2]), atoi(av[3]));
-		i = 0;
-		//printf("%d\n", (char *)memset(av[1], atoi(av[2]), atoi(av[3])));
-		while (i < atoi(av[3]))
-		{
-			printf("%d", *(str + i));
-			i++;
-		}
 	}
 	printf("\n");
 	return (0);
-}
+}*/
