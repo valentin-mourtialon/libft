d45b76c67d4cec97088382d793ad700d1ab5277b
valentin <valentinmourtialon@protonmail.com>
2022-04-19 15:12:14 +0200

    ft_memcpy.c done (norme not checked) and changed <libft.h> into "libft.h" inside ft_bzero.c and ft_memset.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,1 +1,30 @@
-blabla
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	int	i;
+
+	if (src != NULL)
+	{
+		i = 0;
+		while (n-- > 0)
+			((unsigned char *)dest)[i++] = *((unsigned char *)src++);	
+	}
+	return (dest);
+}
+/*
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		printf("src = %s\tdest = %s\n", av[2], av[1]);
+		printf("Original : dest = %s\n", (unsigned char *)memcpy(av[1], av[2], atoi(av[3])));
+		printf("ft_memcpy : dest = %s", (unsigned char *)ft_memcpy(av[1], av[2], atoi(av[3])));
+	}
+	printf("\n");
+	return (0);
+}*/
