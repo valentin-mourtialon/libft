aa70498a3191847a36f91ce5e7a42fc5e4cb69e0
valentin <valentinmourtialon@protonmail.com>
2022-04-20 14:17:39 +0200

    ft_strncmp.c done (norme not checked)

---
+++ ft_strncmp.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && i < n)
+	{
+		if (s1[i] == '\0')
+			return (0);
+		i++;
+	}
+	if (i == n)
+		return (0);
+	else
+		return (s1[i] - s2[i]);
+}
