74eb2c90b0c9816b7605d70c1c55857f68494a4f
valentin <valentinmourtialon@protonmail.com>
2022-04-20 16:22:37 +0200

    ft_atoi.c done (norme not checked)

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+int	ft_atoi(const char *nptr)
+{
+	int	output;
+	int	sign;
+	int	i;
+	int	c;
+
+	output = 0;
+	sign = 1;
+	i = 0;
+	c = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
+		i++;
+	while (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-' && c == 0)
+		{
+			sign *= -1;
+			c++;
+		}
+		else if (c > 0)
+			return (0);
+		i++;
+	}
+	while ('0' <= nptr[i] && nptr[i] <= '9' && nptr[i])
+		output = output * 10 + (nptr[i++] - 48);	
+	return (sign * output);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		const char	*str = NULL;
+			
+		printf("ORIGINAL OUTPUT : %d\n", atoi(av[1]));
+		printf("MY OUTPUT       : %d\n", ft_atoi(av[1]));
+	
+		//printf("ORIGINAL OUTPUT : %d\n", atoi(str));
+		//printf("MY OUTPUT       : %d", ft_atoi(str));
+	}
+	printf("\n");
+	return (0);
+}*/
