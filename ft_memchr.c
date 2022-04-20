c2225b7ec857c4d0a9bff79b99a142aa2213a20a
valentin <valentinmourtialon@protonmail.com>
2022-04-20 14:44:04 +0200

    ft_memchr.c done (norme not checked)

---
+++ ft_memchr.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	if (n == 0)
+		return (NULL);
+	n--;
+	while (n-- > 0)
+	{
+		if (*((unsigned char *)s) == (unsigned char)c)
+			return ((void *)s);
+		s++;
+	}
+	if (*((unsigned char *)s) == (unsigned char)c)
+		return ((void *)s);
+	else
+		return (NULL);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int	main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		printf("s = %s\tc = %s\tn = %s\n", av[1], av[2], av[3]);
+		printf("ORIGINAL memchr : %s\n", (char *)memchr(av[1], atoi(av[2]), atoi(av[3])));
+		printf("MY ft_memchr    : %s", (char *)ft_memchr(av[1], atoi(av[2]), atoi(av[3])));
+	}
+	printf("\n");
+	return (0);
+}*/
