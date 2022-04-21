7783b38f4395d2271729fd623b8acf730f6908b6
valentin <valentinmourtialon@protonmail.com>
2022-04-21 14:52:49 +0200

    ft_substr.c done but not tested : can't find the original one ...(norme not checked)

---
+++ ft_substr.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*output;
+	size_t	i;
+
+	output = malloc(len * sizeof(char));
+	if (!output)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		output[(unsigned int)i] = s[start + (unsigned int)i];
+		i++;
+	}
+	output = '\0';
+	return (output);
+}
+/*
+#include <stdio.h>
+#include <bsd/string.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		printf("ORIGINAL output : %s\n", substr(av[1], atoi(av[2]), atoi(av[3])));
+		printf("MY output       : %s", ft_substr(av[1], atoi(av[2]), atoi(av[3])));		
+	}
+	printf("\n");
+	return (0);
+}*/
