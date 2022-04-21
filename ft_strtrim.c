c2b2390af3dfb9d50d15848362d76a5757d8f5db
valentin <valentinmourtialon@protonmail.com>
2022-04-21 16:55:12 +0200

    ft_strtrim done (norme not checked)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-int	ft_strlen(char *str)
+int	ft_strlen(char const *str)
 {
 	int	len;
 
@@ -10,20 +10,57 @@ int	ft_strlen(char *str)
 	return (len);
 }
 
+int	is_in_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*output;
-	int	s1_length;
-	int	i;
-
-	s1_length = ft_strlen(s1);
-	output = malloc((s1_length - 2) * sizeof(char));
-	i = 1;
-	while (i < s1_length - 1)
+	size_t	s1_length;
+	size_t	nb_char_to_trim;
+	size_t	shift;
+	size_t	i;
+	
+	shift = 0;
+	nb_char_to_trim = 0;
+	if (is_in_set(s1[0], set))
 	{
-		output[i - 1] = s1[i];
+		shift++;
+		nb_char_to_trim++;
+	}
+	s1_length = ft_strlen(s1);
+	if (is_in_set(s1[s1_length - 1], set))
+		nb_char_to_trim++;
+	output = malloc((s1_length - nb_char_to_trim) * sizeof(char));
+	i = 0;
+	while (i < s1_length - nb_char_to_trim)
+	{
+		output[i] = s1[i + shift];
 		i++;
 	}
 	output[i] = '\0';
 	return (output);
 }
+/*
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		printf("OUTPUT : %s", ft_strtrim(av[1], av[2]));
+	}
+	printf("\n");
+	return (0);
+}*/
