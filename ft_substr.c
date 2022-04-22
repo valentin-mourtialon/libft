28c2a6189b90c42a160f58f43e78351f339cdb00
valentin <valentinmourtialon@protonmail.com>
2022-04-22 16:26:22 +0200

    ft_substr.c rectified, add a previously forgotten [i] to output at line 29

--- ft_substr.c
+++ ft_substr.c
@@ -1,31 +1,44 @@
 #include "libft.h"
 
+unsigned int	ft_strlen(char const *s)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*output;
-	size_t	i;
-
+	char		*output;
+	size_t		i;
+	
+	
 	output = malloc(len * sizeof(char));
 	if (!output)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len && s[start + i])
 	{
-		output[(unsigned int)i] = s[start + (unsigned int)i];
+		output[i] = s[start + i];
 		i++;
 	}
-	output = '\0';
+	if (i < len)
+		output[i] = '\0';
 	return (output);
 }
+
 /*
 #include <stdio.h>
-#include <bsd/string.h>
+//#include <bsd/string.h>
 
 int	main(int ac, char **av)
 {
 	if (ac == 4)
 	{
-		printf("ORIGINAL output : %s\n", substr(av[1], atoi(av[2]), atoi(av[3])));
+		//printf("ORIGINAL output : %s\n", substr(av[1], atoi(av[2]), atoi(av[3])));
 		printf("MY output       : %s", ft_substr(av[1], atoi(av[2]), atoi(av[3])));		
 	}
 	printf("\n");
